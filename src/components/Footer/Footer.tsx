0e514a8bf2acb1a5c559d2ee03360602fc342114
Rohoyangu <66417499+Rohoyangu@users.noreply.github.com>
2020-08-12 02:40:36 -0700

    Update Footer.tsx

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,14 +1,6 @@
 import "./scss/index.scss";
 
 import * as React from "react";
-import abc from "../../images/abc.svg";
-import america from "../../images/america.svg";
-import bcd from "../../images/bcd.svg";
-import cici from "../../images/cici.svg";
-import jcb from "../../images/jcb.svg";
-import maestro from "../../images/maestro.svg";
-import master from "../../images/master.svg";
-import visa from "../../images/visa.svg";
 import { SocialMediaIcon } from "..";
 import { SOCIAL_MEDIA } from "../../core/config";
 import Nav from "./Nav";
@@ -25,16 +17,6 @@ const Footer: React.FC = () => (
 
     <Nav />
       <div className="pay">PAYMENT METHOD</div>
-      <div className=" container tra" >
-      <img src={america}  height="28" />
-      <img src={visa}  height="28" />
-      <img src={master}  height="28" />
-      <img src={maestro}  height="28" />
-      <img src={bcd}  height="28" />
-      <img src={jcb} height="28" /> 
-      <img src={abc}  height="28" />
-      <img src={cici}  height="28" />
-  </div>
 );
 
 export default Footer;
